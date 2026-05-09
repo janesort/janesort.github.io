736ed20340c7f9c2abbd86286da2066335ecfedf
Rob <robwalker@outlook.com>
2026-05-08 20:03:08 -0700

    add basic files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# janesort.github.io
+# Jane Sort
+
+https://janesort.github.io/
+
+This is a fork of [Sakura Sort](https://github.com/sakurasort/sakurasort.github.io). Thanks to the contributors there for providing this for us to use.
---
+++ js/op.js
@@ -0,0 +1,20 @@
+﻿function OpenWnd() {
+  var oWindow = window.open("", ""); 
+  with (oWindow.document) {
+    write("<html>");
+    write("<head>");
+    write("<title>TRiDENT Sort: Raw Text Results<\/title>");
+    write("<style>");
+		write("body { background-color: #000; color: #fff; font-family: \"Helvetica Neue\",Helvetica,Arial,sans-serif; font-size: 15px; }");
+		write("<\/style>");
+    write("<\/head>");
+    write("<body>");
+    write(csort5);
+    write("<hr>");
+    write("<input type='button' value='Close' onclick='window.close()'>");
+    write("<hr>");		
+		write("<\/body>");
+    write("<\/html>");
+    close(); 
+  }
+}
